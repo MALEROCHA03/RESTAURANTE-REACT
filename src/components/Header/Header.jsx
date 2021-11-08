d25bae4065419a9db188e631a8d0983a5921439d
juan leon <juanjito.l2020@gmail.com>
2021-11-08 18:45:53 -0500

    reservas y login funcionando

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,13 @@ import { GoThreeBars } from "react-icons/go";
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
 import logo from "../../images/logo.png"
+//import { Login } from "../login/login";
+import { useAuth0 } from '@auth0/auth0-react';
+import { Perfil } from "../login/Perfil";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSignInAlt} from '@fortawesome/free-solid-svg-icons'
+
+const element = <FontAwesomeIcon icon={faSignInAlt} />
 
 
 export const Header = () => {
@@ -16,39 +23,60 @@ export const Header = () => {
   const styleMenu = {
     left: menu ? 0 : "-100%"
   }
+
+  const { loginWithRedirect } = useAuth0();
+
+  const { logout } = useAuth0();
+
+  const { isAuthenticated } = useAuth0(); 
+
   return (
-      <div className={styles.principal}>
-        <header className={styles.header}>
-          <div className={styles.toogle} onClick={toggleMenu}>
-            <GoThreeBars size="45px" />
-          </div>
-          <img className={styles.logo} src={logo} alt="" />
-          <nav className={styles.menu} style={styleMenu}>
-            <ul className={styles.menuList}>
-              <li className={styles.menuItem}>
-                <Link to="/">Inicio</Link>
-              </li>
-              <li className={styles.menuItem}>
-                <Link to="/nosotros">Nosotros</Link>
-              </li>
+    <div className={styles.principal}>
+      <header className={styles.header}>
+        <div className={styles.toogle} onClick={toggleMenu}>
+          <GoThreeBars size="45px" />
+        </div>
+        <img className={styles.logo} src={logo} alt="" />
+        <nav className={styles.menu} style={styleMenu}>
+          <ul className={styles.menuList}>
+            <li className={styles.menuItem}>
+              <Link to="/">Inicio</Link>
+            </li>
+            <li className={styles.menuItem}>
+              <Link to="/nosotros">Nosotros</Link>
+            </li>
+            
+            <li className={styles.menuItem}>
+              <Link to="/menú">Menú</Link>
+            </li>
+            <li className={styles.menuItem}>
+              <Link to="/servicios">Servicios</Link>
+            </li>
+            <li className={styles.menuItem}>
+              <Link to="/contactanos">Contactanos</Link>
+            </li>
 
-              <li className={styles.menuItem}>
-                <Link to="/menú">Menú</Link>
-              </li>
-              <li className={styles.menuItem}>
-                <Link to="/servicios">Servicios</Link>
-              </li>
-              <li className={styles.menuItem}>
-                <Link to="/contactanos">Contactanos</Link>
-              </li>
+            <li className={styles.menuItem}>
+              <Link to="/reservar">Reservar</Link>
+            </li>
+            <li>
 
-              <li className={styles.menuItem}>
-                <Link to="/reservar">Reservar</Link>
-              </li>
-            </ul>
-          </nav>
-
-        </header>
-      </div>
+            {isAuthenticated ?(
+              <>
+             
+              <Perfil/>
+              <button className={styles.ingresar} onClick= {() => logout({ returnTo: window.location.origin } ) }> salir { element } </button>
+              
+            </>
+            ) : (
+              <button className={styles.ingresar} onClick={() => loginWithRedirect()}>Login { element } </button>
+            ) }
+            
+            </li>
+          </ul>
+        </nav>
+      
+      </header>
+    </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.render(
   <React.StrictMode>
+    <Auth0Provider
+      domain="dev-0tsubq18.us.auth0.com"
+      clientId="BK5mvlcaXqqxqzwhzxZi4tOJsPq9SpJw"
+      redirectUri={window.location.origin}
+    >
     <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
